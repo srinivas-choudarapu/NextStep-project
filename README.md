3cf190f1792807cc611e8d78e0bece6253dba500
srinivas-choudarapu <srinuchoudarapu@gamil.com>
2025-09-05 19:25:36 +0530

    added new route for listing out all the questions related to the topic and and the subtopic

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 Routes for the placement preparation section 
 
 Route to show the topics in placemnet i.e. Apptitude, dsa etc is : 
-==>/api/placementprep/topics
+    ==>/api/placementprep/topics
 
 Route to show the sub topics in the main topics like ARRAYS, hashmaps and all is : 
-==>/api/placementprep/topics/dsa
-==>/api/placementprep/topics/aptitude
+    ==>/api/placementprep/topics/dsa
+    ==>/api/placementprep/topics/aptitude
+
+Route to get the questions and questions links by giving subtopic as arrays or Strings or any topic we get the questions oriented to the topic
+    ==>/api/placementprep/topics/dsa/:subtopic
--- models/placementquestion.js
+++ models/placementquestion.js
@@ -1,11 +1,14 @@
+const mongoose = require("mongoose");
+
+
 const PlacementQuestionSchema = new mongoose.Schema({
   topicId: { type: mongoose.Schema.Types.ObjectId, ref: "PlacementTopic", required: true },
 
-  questionText: { type: String, required: true },
-  resourceLink: { type: String },
+  question: { type: String, required: true },
+  link: { type: String },
   difficulty: { type: String, enum: ["Easy", "Medium", "Hard"], default: "Medium" },
   companyTags: [String], // e.g. ["Amazon", "Microsoft"]
 });
 
-const PlacementQuestion = mongoose.model("PlacementQuestion", PlacementQuestionSchema);
+const PlacementQuestion = new mongoose.model("PlacementQuestion", PlacementQuestionSchema);
 module.exports = PlacementQuestion;
