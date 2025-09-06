47af880d3d23706df8a330d0bc839f14f2a44c84
Sathwikgurram <gurramsathwik11@gmail.com>
2025-09-07 00:18:37 +0530

    added branch and sem list

--- routes/examprepRoute.js
+++ routes/examprepRoute.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router =express.Router();
 const Branch= require("../models/branch.js");
+const Branchsub=require("../models/branchsubject.js");
 
 router.get('/branch', async(req,res)=>{
     const blist=await Branch.find({});
     res.json(blist);
 });
 
+//selection of sem and types of notes(pyqs, notes)
+router.get('/:branch/:sem',async(req,res)=>{
+    const {branch,sem,type} =req.params;
+    const subjects=await Branchsub.find({branch:await Branch.find({branchName:branch}),semester:sem},"subject");
+    
+    res.send("sucessfully running ");
+});
 
 
 
